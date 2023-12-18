0659e71900d9ea52c88724a2f05d852a1e95ac5a
10Yogesh <yogeshkharel2@gmail.com>
2023-12-18 08:49:47 +0545

    login and regester

---
+++ frontend/src/redux/actions/user.ts
@@ -0,0 +1,63 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import lwpAxios from "../../config/axiosConfig";
+import { AxiosError } from "axios";
+
+interface LoginData {
+  name?: string;
+  email: string;
+  password: string;
+}
+
+export const loginAsync = createAsyncThunk(
+  "user/login",
+  async (loginData: LoginData) => {
+    try {
+      const response = await lwpAxios.post("/user/login", loginData, {
+        withCredentials: true,
+      });
+      return response.data;
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        throw new Error("Login failed: " + error.response?.data.message);
+      } else {
+        return Promise.reject();
+      }
+    }
+  }
+);
+
+export const createUserAsync = createAsyncThunk(
+  "user/create",
+  async (loginData: LoginData) => {
+    try {
+      const response = await lwpAxios.post("/user/create", loginData, {
+        withCredentials: true,
+      });
+      return response.data;
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        throw new Error("Create failed: " + error.response?.data.message);
+      } else {
+        return Promise.reject();
+      }
+    }
+  }
+);
+
+export const activateUserAsync = createAsyncThunk(
+  "user/active",
+  async (token: string) => {
+    try {
+      const response = await lwpAxios.post(`/user/activation, ${token}`);
+      return response.data;
+    } catch (error: unknown) {
+      if (error instanceof AxiosError) {
+        throw new Error("Login failed: " + error.response?.data.message);
+      } else {
+        return Promise.reject();
+      }
+    }
+  }
+);
+
+
